84aba604d40cd4034ad04add5609dcc2984e65a5
Ducktatorrr <109146941+Ducktatorrr@users.noreply.github.com>
2025-01-19 14:22:53 +0100

    Add player movement

--- main.py
+++ main.py
@@ -15,20 +15,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
     while True:
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-
         # Check if user closes window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # max 60 fps
         dt = clock.tick(60) / 1000
 
 
