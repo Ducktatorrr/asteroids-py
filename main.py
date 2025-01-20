7f60915aa0f387a15473eeef05121345bfed812f
Ducktatorrr <109146941+Ducktatorrr@users.noreply.github.com>
2025-01-20 13:12:02 +0100

    Add asteroid collision detection

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 
 from constants import (
@@ -8,6 +9,7 @@ from constants import (
     ASTEROID_SPAWN_RATE,
     ASTEROID_MAX_RADIUS,
 )
+
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -35,6 +37,11 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
